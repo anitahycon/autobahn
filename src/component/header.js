b431c53eeaf6ac364e206680ab0324ab2070f9e5
anitahycon <anita.g@hyconsoft.in>
2022-06-23 15:56:09 +0530

    header added

---
+++ src/component/header.js
@@ -0,0 +1,10 @@
+const header = () =>{
+    return (
+        <div className="m-0 p-0 w-100 bg-secondary text-white">
+            <h1>header</h1>
+        </div>
+        
+    )
+}
+
+export default header;
